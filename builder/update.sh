05a386f8f3812a7e6fe99f6ecf314a1a4f90454a
Michael Gubbels <mrgubbels@gmail.com>
2017-05-25 00:21:20 -0700

    Update.

--- builder/update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pip uninstall builder -y & pip install . & git add * & git commit -m "Updated library." & git push
---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for argument in "$@"
+do
+	echo "$argument"
+	if [ "$argument" = "git" ]; then
+		echo "GIT"
+	fi;
+done
+
+pip uninstall builder -y
+cd builder
+pip install .
+cd ..
+
+#git add *
+#git commit -m "Updated library."
+#git push
